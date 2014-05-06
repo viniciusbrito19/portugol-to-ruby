cdf6c7a8dc9094d1c6c2f56b26bac4311d69939f
Renan Costa <renan2727@hotmail.com>
2014-05-06 00:06:35 -0300

    Criação da estrutura para salvar em arquivos de forma mais legível e sem repetição

---
+++ Arquivos/saida.rb
@@ -0,0 +1,11 @@
+class nhe
+
+def teste
+	@teste
+end
+
+def teste=(_teste)
+	@teste = _teste
+end
+
+end
--- common.h
+++ common.h
@@ -5,5 +5,6 @@
 extern int yylex(); 
 extern int yyparse(); 
 extern void yyerror(const char* s);
+extern char* yytext;
  
 #endif
