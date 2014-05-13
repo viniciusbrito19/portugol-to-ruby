74597a1dfdfcc8e29950c66b823e94c217699300
Renan Costa <renan2727@hotmail.com>
2014-05-12 22:33:46 -0300

    Correção do bug imprima sem parenteses e função leia() que foi definida de forma mais generica

--- Arquivos/saida.rb
+++ Arquivos/saida.rb
@@ -1,35 +1,4 @@
 class pessoa
 
-	def peso
-		@peso
-	end
-
-	def peso=(_peso)
-		@peso = _peso
-	end
-
-	def altura
-		@altura
-	end
-
-	def altura=(_altura)
-		@altura = _altura
-	end
-
-	def idade
-		@idade
-	end
-
-	def idade=(_idade)
-		@idade = _idade
-	end
-
-	def rg
-		@rg
-	end
-
-	def rg=(_rg)
-		@rg = _rg
-	end
-
+	attr_accessor  :peso, :altura, :idade, :rg
 end
